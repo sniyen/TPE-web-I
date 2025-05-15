17149d23c714f1e36185c5711cfa068f8c7ab6ad
ClariflaroCode <julietawatts.ar@gmail.com>
2025-05-15 12:58:07 -0300

    Tenemos menu hamburguesa VAMOSSSSSSSSSSSS

---
+++ js/menu_desplegable_principal.js
@@ -0,0 +1,5 @@
+document.getElementById("open-menu").addEventListener("click", menuDesplegablePrincipal);
+
+function menuDesplegablePrincipal() {
+    document.querySelector(".nav-menu").classList.toggle("show");
+}
