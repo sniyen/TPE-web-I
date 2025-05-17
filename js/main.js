261958becbe6af2c7dc5a91c0cd3085530535a28
Sofia Niyen <sniyen@alumnos.exa.unicen.edu.ar>
2025-05-17 01:03:15 -0300

    Cambiado el estilo del checkbox, funcional al cambio de tema. Quitados los margin de los li (perdonen a estas chicas por parchear a ojimetro)

--- js/main.js
+++ js/main.js
@@ -3,15 +3,18 @@ switchDarkMode();
 
 function switchDarkMode(){
     let check = document.getElementById("switch-light-dark").checked;
-
+    let icon = document.getElementById("theme-img");
+     
     if(check == true) {
         document.getElementById("fondo").classList.add("oscuro");
         document.getElementById("fondo").classList.remove("claro");
+        icon.src = "../img/solcitouwu.png";
         
         //Añadir a cada elemento su dark mode. Con toggle()
     } else {
         document.getElementById("fondo").classList.add("claro");
         document.getElementById("fondo").classList.remove("oscuro");
+        icon.src = "../img/lunitauwu.png";
         
         //Añadir a cada elemento su light mode. Con toggle()
         //por defecto está en modo claro, porque no está chequeado el checkbox. 
