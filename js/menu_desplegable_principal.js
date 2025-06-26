ccea1387d23cd9ad5f82ba7e6da044b7f8748c24
Sofia Niyen <sniyen@alumnos.exa.unicen.edu.ar>
2025-06-25 23:25:28 -0300

    agregada function iniciar a los js, agregadas choluladas al css

--- js/menu_desplegable_principal.js
+++ js/menu_desplegable_principal.js
@@ -1,5 +1,8 @@
+document.addEventListener("DOMContentLoaded", iniciar);
+function iniciar (){
 document.getElementById("open-menu").addEventListener("click", menuDesplegablePrincipal);
 
 function menuDesplegablePrincipal() {
     document.querySelector(".nav-menu").classList.toggle("show");
+}
 }
